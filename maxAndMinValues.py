b06e73ca17ed1101dc95e61281031d6fd61a9f8f
Daktari <dindijjames@gmail.com>
2017-10-11 10:06:47 +0000

    Corrected the findMinMax function

--- maxAndMinValues.py
+++ maxAndMinValues.py
@@ -1,9 +1,7 @@
-def findMaxMin():
-    input_num = raw_input("Enter numbers separated by space: ")
-    input_list = [int(n) for n in input_num.split()]
-    
-    biggest = max(input_list)
-    smallest = min(input_list)
+def findMaxMin(n):
+    biggest = max(n)
+    smallest = min(n)
+    final_list=[]
     
     if (biggest > smallest):
         final_list = [smallest, biggest]
@@ -12,3 +10,22 @@ def findMaxMin():
     else:
         return False
     return final_list
+
+
+
+
+'''input_num = raw_input("Enter numbers separated by space: ")
+input_list = [int(n) for n in input_num.split()]
+
+def findMaxMin(input_list):
+    biggest = max(input_list)
+    smallest = min(input_list)
+    final_list=[]
+    
+    if (biggest > smallest):
+        final_list = [smallest, biggest]
+    elif (biggest == smallest):
+        final_list = [biggest]
+    else:
+        return False
+    return final_list'''
