6856a5df10bb8c210961680caf4481075b2d7a2e
James Dindi <dindijjames@gmail.com>
2017-10-30 17:30:24 +0300

    Enable project run on Travis-ci

--- maxAndMinValues.py
+++ maxAndMinValues.py
@@ -11,21 +11,3 @@ def findMaxMin(my_list):
         return False
     return final_list
 
-
-
-
-'''input_num = raw_input("Enter numbers separated by space: ")
-input_list = [int(n) for n in input_num.split()]
-
-def findMaxMin(input_list):
-    biggest = max(input_list)
-    smallest = min(input_list)
-    final_list=[]
-    
-    if (biggest > smallest):
-        final_list = [smallest, biggest]
-    elif (biggest == smallest):
-        final_list = [biggest]
-    else:
-        return False
-    return final_list'''
